0475a4d76bc53db8116073ce24c0d3c417a0b85e
SanSYS <i.sansys@gmail.com>
2019-01-14 22:59:02 +0500

    Add threads limits

--- main.go
+++ main.go
@@ -25,18 +25,21 @@ type dnserr struct {
 type cliSettings struct {
 	Domain       string
 	WordlistFile string
+	Threads      int
 }
 
 type semaphore chan bool
 
 var sem semaphore
 var settings cliSettings
+var dnsnames chan string
 
 func main() {
 	printme()
 	parseFlags()
 
 	sem = make(semaphore, 1)
+	dnsnames = make(chan string, settings.Threads)
 	sem <- true
 	solved := make(map[string]bool)
 
@@ -94,14 +97,13 @@ func findByDnsDumpster(wg *sync.WaitGroup, solved map[string]bool) {
 	}
 
 	if httpErr == nil {
-		r = regexp.MustCompile("<td class=\"col-md-4\">([\\w\\d]+)." + settings.Domain + "<br>")
+		r = regexp.MustCompile("col-md-4\">(.+)." + settings.Domain + "<br>")
 		strResp, _ := ioutil.ReadAll(resp.Body)
-		match = r.FindAllString(string(strResp), 1000)
+		match = r.FindAllString(string(strResp), -1)
 
 		for i := 0; i < len(match); i++ {
-			domain := match[i][21 : len(match[i])-4]
-			wg.Add(1)
-			go tryDns(domain, wg, solved)
+			domain := match[i][10 : len(match[i])-4]
+			dnsnames <- domain
 		}
 	}
 }
@@ -115,105 +117,134 @@ func findByWordList(wg *sync.WaitGroup, solved map[string]bool) {
 	}
 	defer file.Close()
 
+	for i := 0; i < settings.Threads; i++ {
+		wg.Add(1)
+		go tryDns(wg, solved)
+	}
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		subdomain := scanner.Text() + "." + settings.Domain
-		wg.Add(1)
-		go tryDns(subdomain, wg, solved)
+		dnsnames <- subdomain
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
+	}
+
+	for i := 0; i < settings.Threads; i++ {
+		dnsnames <- ""
 	}
 }
 
 func parseFlags() {
 	flag.StringVar(&settings.Domain, "d", "", "Set domain scan like '-d ya.ru'")
 	flag.StringVar(&settings.WordlistFile, "w", "wordlist.txt", "Set subdomains dictionary file like '-w wordlist.txt'")
+	flag.IntVar(&settings.Threads, "t", 10, "Set parallelism like '-t 50'")
 
 	flag.Parse()
 
 	if settings.Domain == "" || settings.Domain == "site.ru" {
 		flag.Usage()
-		os.Exit(0)
+		os.Exit(-1)
+	}
+
+	if settings.Threads <= 0 {
+		fmt.Println("-t can not be less than 1")
+		flag.Usage()
+		os.Exit(-1)
 	}
 
 	fmt.Printf("Find subdomains for site:\t%s\r\n", settings.Domain)
 	fmt.Printf("Subdomains dictionary file:\t%s\r\n", settings.WordlistFile)
+	fmt.Printf("Threads:\t%d\r\n", settings.Threads)
 	fmt.Println()
 }
 
-func tryDns(dnsname string, wg *sync.WaitGroup, solved map[string]bool) {
+var cnt int = 0
+
+func tryDns(wg *sync.WaitGroup, solved map[string]bool) {
 	defer wg.Done()
-
-	cname, err := net.LookupCNAME(dnsname)
-
-	<-sem
-	if _, ok := solved[dnsname+cname]; ok {
-		sem <- true
-		return
-	} else {
-		solved[dnsname+cname] = true
-	}
-	sem <- true
-
-	if err != nil {
-		jerr, _ := json.Marshal(err)
-		var derr dnserr
-		json.Unmarshal(jerr, &derr)
-
-		// if jerr != nil && derr.Err == "dnsquery: DNS name does not exist." {
-		// 	return
-		// } else if jerr != nil && derr.Err == "dnsquery: This operation returned because the timeout period expired." {
-		// 	return
-		// } else {
-		// 	fmt.Printf("%s\t%s", cname, err)
-		return
-		//}
-	}
-
-	cname = cname[0 : len(cname)-1]
-
-	iprecords, err := net.LookupIP(dnsname)
-
-	for _, ip := range iprecords {
-		httpClient := &http.Client{Timeout: time.Second * 3}
-		url := fmt.Sprintf("http://%s", dnsname)
-		req, herr := http.NewRequest("GET", url, nil)
-		if herr != nil {
-			fmt.Println(herr)
-			return
-		}
-
-		resp, httpErr := httpClient.Do(req)
-
-		var respCode string
-
-		if httpErr != nil {
-			if resp != nil {
-				respCode = strconv.Itoa(resp.StatusCode)
-			} else {
-				respCode = "?"
+	for {
+		select {
+		case dnsname := <-dnsnames:
+			if dnsname == "" {
+				return
 			}
-		} else {
-			respCode = strconv.Itoa(resp.StatusCode)
-		}
 
-		if dnsname == cname {
-			cname = ""
-		}
+			cname, err := net.LookupCNAME(dnsname)
 
-		linkType := "CNAME"
+			<-sem
+			if _, ok := solved[dnsname]; ok {
+				sem <- true
+				continue
+			} else {
+				solved[dnsname] = true
+			}
+			sem <- true
 
-		if cname == "" {
-			linkType = "HOST"
-		}
+			if err != nil {
+				jerr, _ := json.Marshal(err)
+				var derr dnserr
+				json.Unmarshal(jerr, &derr)
 
-		fmt.Printf("%s\t%s\t%s\t%s\t\t%s\r\n", ip, respCode, linkType, dnsname, cname)
+				// if jerr != nil && derr.Err == "dnsquery: DNS name does not exist." {
+				// 	return
+				// } else if jerr != nil && derr.Err == "dnsquery: This operation returned because the timeout period expired." {
+				// 	return
+				// } else {
+				// 	fmt.Printf("%s\t%s", cname, err)
+				continue
+				//}
+			}
 
-		if cname != "" {
-			wg.Add(1)
-			go tryDns(cname, wg, solved)
+			cname = cname[0 : len(cname)-1]
+
+			iprecords, err := net.LookupIP(dnsname)
+
+			for i, ip := range iprecords {
+				httpClient := &http.Client{Timeout: time.Second * 3}
+				url := fmt.Sprintf("http://%s", dnsname)
+				req, herr := http.NewRequest("GET", url, nil)
+				if herr != nil {
+					fmt.Println(herr)
+					continue
+				}
+
+				resp, httpErr := httpClient.Do(req)
+
+				var respCode string
+
+				if httpErr != nil {
+					if resp != nil {
+						respCode = strconv.Itoa(resp.StatusCode)
+					} else {
+						respCode = "?"
+					}
+				} else {
+					respCode = strconv.Itoa(resp.StatusCode)
+				}
+
+				if dnsname == cname {
+					cname = ""
+				}
+
+				linkType := "CNAME"
+
+				if cname == "" {
+					linkType = "HOST"
+				}
+
+				if i == 0 {
+					fmt.Printf("%s\t%s\t%s\t%s\t\t%s\r\n", ip, respCode, linkType, dnsname, cname)
+				}
+
+				if cname != "" {
+					go func() {
+						dnsnames <- cname
+					}()
+				}
+			}
 		}
 	}
 }
